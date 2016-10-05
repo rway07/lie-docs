324d0e5750592cde2e574650a1190ca6f0eff48d
enrico <kese87@gmail.com>
2016-10-05 21:54:34 +0200

    test caret position

--- public/javascripts/dGogle.js
+++ public/javascripts/dGogle.js
@@ -36,5 +36,52 @@ app.controller('filenameCtrl',function($scope){
   $scope.filename="Unnamed Document";
 });
 
+$(document).ready(function(){
 
+    $("#editable").on('keydown keyup mousedown mouseup',function(e){
+
+        if($(window.getSelection().anchorNode).is($(this))){
+        console.log(0);
+        }else{
+        console.log(window.getSelection().anchorOffset);
+        }
+        });
+
+$.fn.caret = function (begin, end)
+    {
+        if (this.length == 0) return;
+        if (typeof begin == 'number')
+        {
+            end = (typeof end == 'number') ? end : begin;
+            return this.each(function ()
+            {
+                if (this.setSelectionRange)
+                {
+                    this.setSelectionRange(begin, end);
+                } else if (this.createTextRange)
+                {
+                    var range = this.createTextRange();
+                    range.collapse(true);
+                    range.moveEnd('character', end);
+                    range.moveStart('character', begin);
+                    try { range.select(); } catch (ex) { }
+                }
+            });
+        } else
+        {
+            if (this[0].setSelectionRange)
+            {
+                begin = this[0].selectionStart;
+                end = this[0].selectionEnd;
+            } else if (document.selection && document.selection.createRange)
+            {
+                var range = document.selection.createRange();
+                begin = 0 - range.duplicate().moveStart('character', -100000);
+                end = begin + range.text.length;
+            }
+            return { begin: begin, end: end };
+        }
+    }
+
+});
 
